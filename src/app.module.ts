129d66a642998999cb5493da70d9e2eef272c70e
Pablo Pessanha <pablopessanh@gmail.com>
2021-07-15 17:26:55 -0300

    add graphql and schema modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController, TestController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController, TestController],
   providers: [AppService],
 })
