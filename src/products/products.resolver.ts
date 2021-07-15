e523a2d48fdc01c4e00020a35267f11ef06fac4a
Pablo Pessanha <pablopessanh@gmail.com>
2021-07-15 19:59:56 -0300

    add new product method

--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -1,4 +1,5 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver, Mutation } from '@nestjs/graphql';
+import { CreateProductInput } from 'src/dto/create-product.input';
 import { Product } from './product.entity';
 import { ProductsService } from './products.service';
 
@@ -9,5 +10,12 @@ export class ProductsResolver {
   @Query((returns) => [Product])
   products(): Promise<Product[]> {
     return this.productsService.findAll();
+  }
+
+  @Mutation((returns) => Product)
+  newProduct(
+    @Args('createProductInput') createProductInput: CreateProductInput,
+  ): Promise<Product> {
+    return this.productsService.newProduct(createProductInput);
   }
 }
